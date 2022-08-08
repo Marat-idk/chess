a6f6815854a4bb9043d71da3667c5c90f9a5031a
Enkidu <molodoy.dracon@gmail.com>
2022-08-08 10:11:35 +0300

    Удален мусор

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,5 @@ using namespace Chess;
 
 int main(){
     ChessBoard temp;
-    //temp.StartGame();
-
-    std::vector<int> v = {1, 2, 3, 4, 1, 1, 2, 2, 3, 3};
-    v.erase(std::remove(v.begin(), v.end(), 2), v.end());
-    for(const int &value: v){
-        std::cout << value << " ";
-    }
-    std::cout << std::endl;
+    temp.StartGame();
 }
